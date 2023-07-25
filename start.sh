917d483f038c0760a28af0b61a73231e2211cbd3
Proffesorbro <101329641+Proffesorbro@users.noreply.github.com>
2023-07-25 21:40:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Proffesorbro/op.git    /op
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /op
-fi
-cd /op
 pip3 install -U -r requirements.txt
 echo "Starting Bot..."
 python3 bot.py
